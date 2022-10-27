269c70503452cd97d1a12f7a75583e2d622bc982
Angelo Cassio Ramos Xavier <cassio.ramos@centralinf.com.br>
2022-10-27 16:53:21 -0300

    Initial Commit

---
+++ CustumerApp/CustumerApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustumerApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
